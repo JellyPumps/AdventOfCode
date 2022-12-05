55d18ea92d5db0fc6bd05e4341aeecaaf9cb7f29
JellyPumps <sarthakrai3406@gmail.com>
2022-12-05 17:57:30 +0530

    IT WORKS SOMEHOW?!

--- 2022/Day 5/main.py
+++ 2022/Day 5/main.py
@@ -7,7 +7,7 @@ stacks_count=[]; stacks_raw  =[]; stacks=[];
 instructions=[];
 raw         =[]
 
-with open(testInp) as f:
+with open(aocdInp) as f:
     for line in f:
         raw.append(line.strip('\n'))
 f.close()
@@ -24,13 +24,14 @@ for i in range(len(stacks_count)):
     stacks_count[i]=int(stacks_count[i])
 noStacks=stacks_count[-1]
 for i in range(len(stacks_raw)):
-    stacks_raw[i]=[stacks_raw[i][x:x+noStacks] for x in range(0,len(stacks_raw[i]),noStacks+1)]
+    stacks_raw[i]=[stacks_raw[i][x:x+noStacks] for x in range(0,len(stacks_raw[i]),noStacks-2)]
 for i in range(noStacks):
     app=[x[i] for x in stacks_raw]
     stacks.append(app)
-for x in range(len(stacks)):
-    for y in range(len(stacks[x])):
-        stacks[x][y]=re.sub(re.compile(r'\s+'),'x',stacks[x][y])
+for i in range(len(stacks)):
+    for y in range(len(stacks[i])):
+        stacks[i][y]=re.sub(re.compile(r'\s+'),'',stacks[i][y])
+    stacks[i]=[x for x in stacks[i] if x]
 
 #PARSE INSTRUCTIONS
 for i in range(len(instructions)):
@@ -41,14 +42,18 @@ for i in range(len(instructions)):
     numberToMove=int(instructions[i][0])
     moveFromStck=int(instructions[i][1])-1
     stckDestintn=int(instructions[i][2])-1
-    for x in range(numberToMove):  
-        print("x")      
-        for cargo in stacks[moveFromStck]:
-            if cargo != 'x':
-                indx=stacks[moveFromStck].index(cargo)
-                for cargo_x in stacks[stckDestintn]:
-                    if cargo_x != 'x':
-                        indy=stacks[stckDestintn].index(cargo_x)-1
-                        stacks[moveFromStck][indx] #keeps printing 'x'?
-print(stacks)
+
+    print(moveFromStck)
+
+    for x in range(numberToMove):
+        targ=stacks[moveFromStck]        
+        dest=stacks[stckDestintn]
+        dest.insert(0,targ[0])
+        targ.pop(0)
+
+for i in range(len(stacks)):
+    print(*stacks[i][0],sep='',end='')
+
+#print(stacks)
+print()
 print("Number of stacks:",noStacks)
