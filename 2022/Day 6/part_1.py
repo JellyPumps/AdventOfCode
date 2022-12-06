b352b847210f4057ac28c3eae21c4e50af1dc51a
JellyPumps <sarthakrai3406@gmail.com>
2022-12-06 11:48:56 +0530

    not so spaghetti today!

---
+++ 2022/Day 6/part_1.py
@@ -0,0 +1,23 @@
+string="bvwbjplbgvbhsrlpgdmjqwftvncz"
+spliced=[]
+processed=0
+count=0
+
+with open("2022/inputs/day_6_input.txt") as file:
+    while True:
+        c=file.read(1)
+        if not c:
+            break
+        spliced.append(c)
+for c in range(len(spliced)):
+    s=spliced
+    try: temp=[s[c],s[c+1],s[c+2],s[c+3]]
+    except IndexError as e: print(e); exit()
+    tmpstr=""
+    for e in temp:
+        tmpstr+=e
+    if len(set(tmpstr))==len(tmpstr):
+        tmpc=tmpstr[-1]; processed=tmpstr.index(tmpc)+1+count; break
+    count+=1
+
+print(processed)
