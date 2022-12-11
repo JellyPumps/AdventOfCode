3539bb422ac1fcee99c34587116b8729c736e967
JellyPumps <sarthakrai3406@gmail.com>
2022-12-11 06:07:14 +0530

    forgot to commit day 10 yesterday

--- 2022/Day 10/main.py
+++ 2022/Day 10/main.py
@@ -1,11 +1,41 @@
-from comp import *
 test="2022/inputs/test.txt"
 aocd="2022/inputs/day_10_input.txt"
-target=test
+target=aocd
+cycles=1
+regx  =1
+signal=0
+catch =[20,60,100,140,180,220]
+wonder=[]
+
+def sumeru()->None:
+    global cycles,signal,regx
+    if cycles in catch: signal+=cycles*regx
+    else              : 0
 with open(target) as f:
     for line in f:
         line=line.rstrip()
-        cont=line.split()
-        if cont[0]=='noop': noop()
-        if cont[0]=='addx': addx(int(cont[1]))
-peepee()
+        wonder.append(line)
+        cycles+=1
+        if 'addx' in line:
+            sumeru()
+            cycles+=1
+            regx+=int(line.split()[1])
+        sumeru()
+f.close()
+
+regx  =1
+cycles=0
+pepsi=lambda cycles,regx: '■' if abs(cycles-regx)<=1 else '◦'
+crt=''
+tond=[]
+
+for i in wonder:
+    crt+=pepsi(cycles,regx); cycles=(cycles+1)%40
+    if 'addx' in i:
+        crt+=pepsi(cycles,regx)
+        cycles=(cycles+1)%40
+        regx+=int(i.split()[1])
+print(f"Part 1: {signal}")
+for row in range(0,6): print(crt[row*40:(row+1)*40])
+
+
